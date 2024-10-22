b8d0b0f22df4d663dc5d25d7bfb4538307545d94
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-22 09:40:18 +0300

    t10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,7 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """update all topics of a school document based on the name: """
-    mongo_collection.update(
+    mongo_collection.update_many(
         {name: name},
         {"$set": {topics: topics}},
     )
