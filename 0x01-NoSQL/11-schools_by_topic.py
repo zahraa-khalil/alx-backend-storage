4fd1d99c72bf1029e89d2362ef6e027cda58d902
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-22 09:54:55 +0300

    t11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""DB implementation"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """function that returns the list of school having a specific topic """
+    return mongo_collection.find({"topics": topic})
