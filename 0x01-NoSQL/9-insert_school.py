090cb874d2991f843764efb9af46dcbd362ee558
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-22 09:34:56 +0300

    update

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,4 +5,4 @@
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection """
     new_documents = mongo_collection.insert_one(kwargs)
-    return new_documents
+    return new_documents.inserted_id
