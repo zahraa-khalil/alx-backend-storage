3f3e80f9fc2d673d89e6fa6988b8201c09958f06
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-24 05:24:32 +0300

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,8 @@
 """Redis basic"""
 
 
-from typing import Union
+import string
+from typing import Callable, Union
 import uuid
 import redis
 
@@ -19,3 +20,19 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        """Get data from the cache"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''Retrieves an integer value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: int(x))
